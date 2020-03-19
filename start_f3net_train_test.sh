d7b4fdbcd1d106623ff790f97b177c25705a5f29
Sam_cody <sambitskhr@gmail.com>
2020-03-19 12:59:19 +0530

    adding bash script to train f3net.

---
+++ start_f3net_train_test.sh
@@ -0,0 +1,24 @@
+
+
+
+!pip install torch==1.3.1+cu100 torchvision==0.4.2+cu100 -f https://download.pytorch.org/whl/torch_stable.html
+!pip install tensorboardX
+
+
+
+
+git clone https://github.com/NVIDIA/apex
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./apex
+
+# !cp -a /content/datasets/imgs/pascal/. /content/F3Net/data/PASCAL-S/image/
+# !cp -a /content/datasets/masks/pascal/. /content/F3Net/data/PASCAL-S/mask/
+
+
+
+
+!wget https://download.pytorch.org/models/resnet50-19c8e357.pth
+!mv /content/resnet50-19c8e357.pth /content/F3Net/res/
+
+
+!python /content/F3Net/src/train.py
+!python /content/F3Net/src/test.py
